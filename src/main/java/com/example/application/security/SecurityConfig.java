a8e3d36c2aa16287b3eec009c3ca63753caf107a
Adan Vinicius <viniciusadan123@gmail.com>
2024-05-23 21:06:11 -0300

    @ad4nv1[Task: Criando redirecionamento de rotas]

--- src/main/java/com/example/application/security/SecurityConfig.java
+++ src/main/java/com/example/application/security/SecurityConfig.java
@@ -1,19 +1,16 @@
 package com.example.application.security;
 
 import com.example.application.provider.CustomAuthenticationProvider;
+import com.example.application.provider.CustomAuthenticationSuccessHandler;
 import com.example.application.view.LoginView;
 import com.vaadin.flow.spring.security.VaadinWebSecurity;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
@@ -25,9 +22,19 @@ public class SecurityConfig extends VaadinWebSecurity {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth ->
-                auth.requestMatchers(
-                        AntPathRequestMatcher.antMatcher(HttpMethod.GET, "/images/*.png")).permitAll());
+        http.authorizeHttpRequests(auth -> {
+            auth
+                    .requestMatchers(new AntPathRequestMatcher("/images/*.png", "GET")).permitAll()
+                    .requestMatchers("/aluno/**").hasAuthority("ROLE_ALUNO")
+                    .requestMatchers("/gestor/**").hasAuthority("ROLE_GESTOR")
+                    .anyRequest().authenticated();
+        });
+
+        http.formLogin()
+                .loginPage("/login")
+                .successHandler(customAuthenticationSuccessHandler())
+                .permitAll();
+
         super.configure(http);
         setLoginView(http, LoginView.class);
     }
@@ -35,5 +42,9 @@ public class SecurityConfig extends VaadinWebSecurity {
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.authenticationProvider(customAuthenticationProvider);
+    }
+
+    private AuthenticationSuccessHandler customAuthenticationSuccessHandler() {
+        return new CustomAuthenticationSuccessHandler();
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,21 +1,21 @@
 INSERT INTO ALUNO (ID, VERSION, MATRICULA, NOME, EMAIL, SERIE, TURMA, SENHA) VALUES
-(1, 1, '20232GTIPL0312', 'João Silva', 'joao.silva@example.com', '1° ANO', 'B', 'senha123'),
-(2, 1, '20232GTIPL0419', 'Maria Eduarda', 'dudaazv@example.com', '2° ANO', 'A', 'jds566'),
-(3, 1, '20232GTIPL0718', 'Rogerio Fernandes', 'rogerio678@example.com', '3° ANO', 'C', 'rgr789'),
-(4, 1, '2023GTIPL0417', 'Mercio Luiz', 'mluiz89@example.com', '2° ANO', 'A', 'mercio209'),
-(5, 1, '20232GTIPL457', 'Julia Ribeiro', 'juribeiro16@example.com', '1° ANO', 'B', 'ju123'),
-(6, 1, '20232GTIPL457', 'Julia Ribeiro', 'juribeiro16@example.com', '1° ANO', 'B', 'ju123'),
-(7, 1, '2024GTIZ1234', 'Gustavo Santos', 'gustavosantos@example.com', '2° ANO', 'A', 'gs456'),
-(8, 1, '20242GTIX4321', 'Amanda Lima', 'amandalima@example.com', '3° ANO', 'B', 'al789'),
-(9, 1, '20244GTI1234', 'Rafaela Souza', 'rafaelasouza@example.com', '1° ANO', 'C', 'rs321'),
-(10, 1, '20246GTIV5432', 'Matheus Oliveira', 'matheusoliveira@example.com', '2° ANO', 'A', 'mo654'),
-(11, 1, '20248GTI1234', 'Larissa Costa', 'larissacosta@example.com', '3° ANO', 'B', 'lc987'),
-(12, 1, '20250GTI9876', 'Fernanda Silva', 'fernandasilva@example.com', '1° ANO', 'D', 'fs123'),
-(13, 1, '20252GTI5678', 'Lucas Rodrigues', 'lucasrodrigues@example.com', '2° ANO', 'C', 'lr456'),
-(14, 1, '20254GTI4321', 'Carolina Oliveira', 'carolinaoliveira@example.com', '3° ANO', 'A', 'co789'),
-(15, 1, '20256GTI9876', 'Pedro Almeida', 'pedroalmeida@example.com', '1° ANO', 'B', 'pa321'),
-(16, 1, '20258GTI5678', 'Mariana Pereira', 'marianapereira@example.com', '2° ANO', 'D', 'mp654'),
-(17, 1, '20260GTI4321', 'Rodrigo Martins', 'rodrigomartins@example.com', '3° ANO', 'C', 'rm987');
+(1, 1, '20232GTIPL0312', 'João Silva', 'joao.silva@example.com', '1° ANO', 'B', 'senha123', true),
+(2, 1, '20232GTIPL0419', 'Maria Eduarda', 'dudaazv@example.com', '2° ANO', 'A', 'jds566', true),
+(3, 1, '20232GTIPL0718', 'Rogerio Fernandes', 'rogerio678@example.com', '3° ANO', 'C', 'rgr789', true),
+(4, 1, '2023GTIPL0417', 'Mercio Luiz', 'mluiz89@example.com', '2° ANO', 'A', 'mercio209', true),
+(5, 1, '20232GTIPL457', 'Julia Ribeiro', 'juribeiro16@example.com', '1° ANO', 'B', 'ju123', true),
+(6, 1, '20232GTIPL457', 'Julia Ribeiro', 'juribeiro16@example.com', '1° ANO', 'B', 'ju123', true),
+(7, 1, '2024GTIZ1234', 'Gustavo Santos', 'gustavosantos@example.com', '2° ANO', 'A', 'gs456', true),
+(8, 1, '20242GTIX4321', 'Amanda Lima', 'amandalima@example.com', '3° ANO', 'B', 'al789', true),
+(9, 1, '20244GTI1234', 'Rafaela Souza', 'rafaelasouza@example.com', '1° ANO', 'C', 'rs321', true),
+(10, 1, '20246GTIV5432', 'Matheus Oliveira', 'matheusoliveira@example.com', '2° ANO', 'A', 'mo654', true),
+(11, 1, '20248GTI1234', 'Larissa Costa', 'larissacosta@example.com', '3° ANO', 'B', 'lc987', true),
+(12, 1, '20250GTI9876', 'Fernanda Silva', 'fernandasilva@example.com', '1° ANO', 'D', 'fs123', false),
+(13, 1, '20252GTI5678', 'Lucas Rodrigues', 'lucasrodrigues@example.com', '2° ANO', 'C', 'lr456', false),
+(14, 1, '20254GTI4321', 'Carolina Oliveira', 'carolinaoliveira@example.com', '3° ANO', 'A', 'co789', false),
+(15, 1, '20256GTI9876', 'Pedro Almeida', 'pedroalmeida@example.com', '1° ANO', 'B', 'pa321', false),
+(16, 1, '20258GTI5678', 'Mariana Pereira', 'marianapereira@example.com', '2° ANO', 'D', 'mp654', false),
+(17, 1, '20260GTI4321', 'Rodrigo Martins', 'rodrigomartins@example.com', '3° ANO', 'C', 'rm987', false);
 
 
 INSERT INTO GESTOR (ID, VERSION, MATRICULA, NOME, EMAIL, SENHA) VALUES
@@ -23,8 +23,4 @@ INSERT INTO GESTOR (ID, VERSION, MATRICULA, NOME, EMAIL, SENHA) VALUES
 (2, 1, '20232GTIP5465454', 'Maria Florinda', 'turmadochavessbt@example.com', 'chaves7852'),
 (3, 1, '20232GTIPL07896255', 'Carlos da oficina', 'pneusnovos@example.com', 'pneus8954'),
 (4, 1, '2023GTIPL7895', 'Hebert Conde', 'so@example.com', 'brega7895');
-
-
-
-
 
